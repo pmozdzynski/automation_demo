39edcc805ccb9bb11be0bf2011728d42685c1458
PM <piotrek@kcn.pl>
2025-10-07 21:38:24 +0200

    Adding runner

--- main.tf
+++ main.tf
@@ -1,37 +1,68 @@
-  terraform {
-    required_providers {
-      hcloud = {
-        source  = "hetznercloud/hcloud"
-        version = "~> 1.54.0"
-      }
+terraform {
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~> 1.54.0"
     }
   }
+}
 
-  provider "hcloud" {
-    # Terraform automatically picks up HCLOUD_TOKEN from the environment
-  }
+provider "hcloud" {
+  # Terraform automatically picks up HCLOUD_TOKEN from the environment
+}
 
-  resource "hcloud_server" "web" {
-    name        = "web-automation-gibtelecom"
-    image       = "docker-ce"  # x86_64 supported
-    server_type = "cpx11"             # smallest AMD server
-    ssh_keys    = ["generic-key"]     # Existing Hetzner key
-    location    = "hel1"              # Helsinki
-  }
+# ------------------------------
+# Web server (Docker + Nginx)
+# ------------------------------
+resource "hcloud_server" "web" {
+  name        = "web-automation-gibtelecom"
+  image       = "docker-ce"        # x86_64 supported
+  server_type = "cpx11"            # smallest AMD server
+  ssh_keys    = ["generic-key"]    # Existing Hetzner key
+  location    = "hel1"             # Helsinki
+}
 
-# Generate a local inventory file for Ansible with SSH options to ignore host key checking ( when hetzner provided new server ) 
+# ------------------------------
+# GitHub self-hosted runner
+# ------------------------------
+resource "hcloud_server" "runner" {
+  name        = "github-runner"
+  image       = "docker-ce"
+  server_type = "cpx11"
+  ssh_keys    = ["generic-key"]
+  location    = "hel1"
+}
+
+# ------------------------------
+# Generate Ansible inventory
+# ------------------------------
 resource "local_file" "ansible_inventory" {
   content = <<EOT
 [web]
 ${hcloud_server.web.ipv4_address} ansible_user=root ansible_ssh_private_key_file=~/.ssh/id_rsa ansible_ssh_common_args='-o StrictHostKeyChecking=no'
+
+[runner]
+${hcloud_server.runner.ipv4_address} ansible_user=root ansible_ssh_private_key_file=~/.ssh/id_rsa ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 EOT
   filename = "${path.module}/inventory"
 }
 
-  output "ipv4" {
-    value = hcloud_server.web.ipv4_address
-  }
+# ------------------------------
+# Outputs
+# ------------------------------
+output "web_ipv4" {
+  value = hcloud_server.web.ipv4_address
+}
 
-  output "ipv6" {
-    value = hcloud_server.web.ipv6_address
-  }
+output "web_ipv6" {
+  value = hcloud_server.web.ipv6_address
+}
+
+output "runner_ipv4" {
+  value = hcloud_server.runner.ipv4_address
+}
+
+output "runner_ipv6" {
+  value = hcloud_server.runner.ipv6_address
+}
+
